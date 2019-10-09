61119d8e0b520fce8ab9bacf40220d3878df590e
rinex20 <rinex20@me.com>
2019-10-09 14:05:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
+!#/bin/sh
 
+git clone https://github.com/zfl9/ss-tproxy
+cd ss-tproxy
+chmod +x ss-tproxy
+cp -af ss-tproxy /usr/local/bin
+mkdir -p /etc/ss-tproxy
+cp -af ss-tproxy.conf gfwlist* chnroute* /etc/ss-tproxy
+cp -af ss-tproxy.service /etc/systemd/system # 可选，安装 service 文件
